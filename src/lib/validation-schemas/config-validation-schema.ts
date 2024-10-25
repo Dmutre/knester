c415ba835fdc574a54ce9f4960999e9f77919e43
Dmutre <104783173+Dmutre@users.noreply.github.com>
2024-10-25 17:22:41 +0200

    feat(config): updated validation schema

--- src/lib/validation-schemas/config-validation-schema.ts
+++ src/lib/validation-schemas/config-validation-schema.ts
@@ -2,5 +2,13 @@ import * as Joi from 'joi';
 
 export const configValidationSchema = Joi.object({
   PORT: Joi.number().required(),
+  JWT_SECRET: Joi.string().required(),
+  ACCESS_TTL: Joi.string().required(),
+  JWT_TTL: Joi.string().required(),
+  DATABASE_USER: Joi.string().required(),
+  DATABASE_PASSWORD: Joi.string().required(),
+  DATABASE_HOST: Joi.string().required(),
+  DATABASE_PORT: Joi.string().required(),
+  DATABASE_NAME: Joi.string().required(),
   DATABASE_URL: Joi.string().required(),
 });
